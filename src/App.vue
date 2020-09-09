706030d6a96bb36b7a6f0cb22f769eb0d3316469
Eduardo Nicolli <edu.souza.n@hotmail.com>
2020-09-09 03:37:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+
+ 
+    <div class = "bg-light">
+
+      <h1 class="d-flex justify-content-center pt-2 pb-3">Vue Pokedex</h1>
+
+          <form class="form-inline justify-content-center pt-2 pb-3">
+          <input class="form-control form-control-lg" type="text" placeholder="Find Pokemon" v-model="busca">
+          </form>
+
+      <div class= "row d-flex justify-content-center pb-5 mt-3">
+    
+
+  <div v-for="(name, index ) in resultadoBusca" :key="name.url"> 
+   <Pokemon :name="name.name" :index="index+1" :url="name.url" />
+
+
+            
+          </div>
+        </div>
+      </div>
+   
+    
+  
+</template>
+
+<script>
+import axios from 'axios'
+import Pokemon from './components/Pokemon'
+
+export default {
+  name: 'App',  
+  data(){
+    return {
+      pokemons: [],
+      busca: ' '
+    }
+  },
+  created: function(){ //call it when created (useEffect?)
+  axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(
+    res => {
+      
+      this.pokemons= res.data.results
+      console.log(this.pokemons)
+    }
+  )
+    },
+  components:{
+    Pokemon
+  },
+
+  computed:{
+    resultadoBusca: function(){
+      if (this.busca == '' || this.busca == ' '){
+        return this.pokemons
+      }
+      else{
+        return this.pokemons.filter(poke => poke.name.includes(this.busca))
+        
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,71 @@
+<template>
+
+        <div class="col-sm mb-4">
+        <div>
+   
+               
+                <div class="card " style="width: 18rem;">
+            <img  :src='pokemonSprite' class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title d-flex justify-content-center"><b class="pr-2"> #{{urlPokeId}} </b> {{name | upper}}</h5>
+                
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Type: {{pokemonType}} </li>
+                
+            </ul>
+            
+           
+</div>
+    
+</div>
+</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    
+    created: function() {
+        axios.get(this.url).then(
+             res => {
+                
+                this.pokemonType = res.data.types[0].type.name
+                this.pokemonSprite = res.data.sprites.front_default
+                      
+                      console.log(this.urlPokeId) 
+                
+    }
+  )
+    },
+    data: function(){
+        return{
+            pokemonSprite: '',
+            pokemonType: '',
+            urlPokeId: this.url.match(/\d+/g).slice(-1).join("")
+            
+        }
+
+    },
+    props: {
+        name: String,
+        index: Number,
+        url: String,
+    },
+
+    filters: {
+        upper: function (value){
+            let upperLetter = value[0].toUpperCase() + value.slice(1)
+            return upperLetter
+             
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
